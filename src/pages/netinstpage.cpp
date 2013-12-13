7e1e212cf40865215106d9aef832571c328e25f7
demmm <anke62@gmail.com>
2013-12-12 22:21:13 -0500

    lower, less wide widget

--- src/pages/netinstpage.cpp
+++ src/pages/netinstpage.cpp
@@ -27,7 +27,7 @@ NetInstPage::NetInstPage(QWidget *parent)
     m_groups["printing"]        = {i18n("Printing"),        i18n("Print much?")};
     m_groups["multimedia"]      = {i18n("Multimedia"),      i18n("Music and video players")};
     m_groups["gtk-integration"] = {i18n("Gtk Integration"), i18n("Integrate Gtk applications with plasma")};
-    m_groups["misc"]            = {i18n("Miscelaneous"),    i18n("Useful tools and apps")};
+    m_groups["misc"]            = {i18n("Miscellaneous"),   i18n("Useful tools and apps")};
 }
 
 void NetInstPage::createWidget()
